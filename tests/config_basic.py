a1332e30e048ec160a36fd2309d0708a5233ae24
Artur Siekielski <artur.siekielski@vhex.net>
2018-06-09 10:52:10 +0200

    Use local Postgres authentication for tests.

--- tests/config_basic.py
+++ tests/config_basic.py
@@ -2,36 +2,38 @@ import os.path
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
+_postgres_dsn = 'dbname=mtest1'
+
 DATABASES = {
         'default': {
             'migrations_dir': os.path.join(BASE_DIR, 'migrations1'),
             'engine': 'postgres',
-            'dsn': 'host=127.0.0.1 dbname=mtest1',
+            'dsn': _postgres_dsn,
             'after_sync': 'pg_dump mtest1 > /tmp/mtest1.sql',
         },
 
         'error': {
             'migrations_dir': os.path.join(BASE_DIR, 'migrations_error'),
             'engine': 'postgres',
-            'dsn': 'host=127.0.0.1 dbname=mtest1',
+            'dsn': _postgres_dsn,
         },
 
         'extensions1': {
             'migrations_dir': os.path.join(BASE_DIR, 'extensions1'),
             'engine': 'postgres',
-            'dsn': 'host=127.0.0.1 dbname=mtest1',
+            'dsn': _postgres_dsn,
         },
 
         'migrate_args': {
             'migrations_dir': os.path.join(BASE_DIR, 'migrate_args'),
             'engine': 'postgres',
-            'dsn': 'host=127.0.0.1 dbname=mtest1',
+            'dsn': _postgres_dsn,
         },
 
         'different_schema': {
             'migrations_dir': os.path.join(BASE_DIR, 'different_schema'),
             'engine': 'postgres',
-            'dsn': 'host=127.0.0.1 dbname=mtest1',
+            'dsn': _postgres_dsn,
             'migration_table': 'hooli.migration'
         },
 
